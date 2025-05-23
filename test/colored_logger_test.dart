30860ba9318b5e68f5f74f558e4cb48a4c302070
Venh <venh.dev@gmail.com>
2025-05-23 17:12:39 +0700

    add test

--- test/colored_logger_test.dart
+++ test/colored_logger_test.dart
@@ -24,39 +24,33 @@ void main() {
 
   group('Custom Logging with Color Codes', () {
     test('normal color code', () {
-      ColoredLogger.custom('Test normal message',
-          colorCode: 'normal', prefix: '[NORMAL] ');
+      ColoredLogger.custom('Test normal message', colorCode: 'normal', prefix: '[NORMAL] ');
     });
 
     test('custom color codes', () {
-      ColoredLogger.custom('Test blue message',
-          colorCode: 'blue', prefix: '[BLUE] ');
-      ColoredLogger.custom('Test red message',
-          colorCode: 'red', prefix: '[RED] ');
-      ColoredLogger.custom('Test green message',
-          colorCode: 'green', prefix: '[GREEN] ');
-      ColoredLogger.custom('Test yellow message',
-          colorCode: 'yellow', prefix: '[YELLOW] ');
+      ColoredLogger.custom('Test blue message', colorCode: 'blue', prefix: '[BLUE] ');
+      ColoredLogger.custom('Test red message', colorCode: 'red', prefix: '[RED] ');
+      ColoredLogger.custom('Test green message', colorCode: 'green', prefix: '[GREEN] ');
+      ColoredLogger.custom('Test yellow message', colorCode: 'yellow', prefix: '[YELLOW] ');
+    });
+
+    test('long string', () {
+      final String longString = 'A' * 100000; // 100,000 'A's
+      ColoredLogger.custom(longString, colorCode: 'green', prefix: '[BLUE] ');
     });
   });
 
   group('Custom Logging with ANSI Codes', () {
     test('single ANSI code', () {
-      ColoredLogger.custom('Test bold text',
-          ansiCode: [AnsiCode.bold], prefix: '[BOLD] ');
-      ColoredLogger.custom('Test dim text',
-          ansiCode: [AnsiCode.dim], prefix: '[DIM] ');
-      ColoredLogger.custom('Test underline text',
-          ansiCode: [AnsiCode.underline], prefix: '[UNDERLINE] ');
+      ColoredLogger.custom('Test bold text', ansiCode: [AnsiCode.bold], prefix: '[BOLD] ');
+      ColoredLogger.custom('Test dim text', ansiCode: [AnsiCode.dim], prefix: '[DIM] ');
+      ColoredLogger.custom('Test underline text', ansiCode: [AnsiCode.underline], prefix: '[UNDERLINE] ');
     });
 
     test('background colors', () {
-      ColoredLogger.custom('Test bg red',
-          ansiCode: [AnsiCode.bgRed], prefix: '[BG_RED] ');
-      ColoredLogger.custom('Test bg blue',
-          ansiCode: [AnsiCode.bgBlue], prefix: '[BG_BLUE] ');
-      ColoredLogger.custom('Test bg green',
-          ansiCode: [AnsiCode.bgGreen], prefix: '[BG_GREEN] ');
+      ColoredLogger.custom('Test bg red', ansiCode: [AnsiCode.bgRed], prefix: '[BG_RED] ');
+      ColoredLogger.custom('Test bg blue', ansiCode: [AnsiCode.bgBlue], prefix: '[BG_BLUE] ');
+      ColoredLogger.custom('Test bg green', ansiCode: [AnsiCode.bgGreen], prefix: '[BG_GREEN] ');
     });
 
     test('combined ANSI codes', () {
@@ -83,12 +77,6 @@ void main() {
   });
 
   /// Print Long String
-  void printLongString(String text) {
-    final RegExp pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
-    pattern
-        .allMatches(text)
-        .forEach((RegExpMatch match) => print(match.group(0)));
-  }
 
   group('long print', () {
     test('long print', () {
