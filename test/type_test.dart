633746367913c9c7f76b4a3863b1bb401e3971c7
Venh <venh.dev@gmail.com>
2025-06-08 02:30:43 +0700

    add test

--- test/type_test.dart
+++ test/type_test.dart
@@ -1,5 +1,7 @@
+import 'dart:developer';
 import 'dart:io';
 
+import 'package:colored_logger/colored_logger.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -8,5 +10,12 @@ void main() {
     final ansi = stdout.supportsAnsiEscapes;
     print('supportsAnsiEscapes: $ansi');
     print('type: $type');
+  });
+
+  test('--', () {
+    final str =
+        '''curl -i -X GET -H "Content-Type: application/json" -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS1kc3AubmV3d2ViLnZuL2NsaWVudC9hdXRoL3Rva2VuLWJ5LXVzZXItY29kZS9LSEFDSExFIiwiaWF0IjoxNzQ4OTczMzMyLCJleHAiOjE3ODA1MDkzMzIsIm5iZiI6MTc0ODk3MzMzMiwianRpIjoiU0JBT1pOMXM4ZFgxd0pobyIsInN1YiI6IjgiLCJwcnYiOiI4N2UwYWYxZWY5ZmQxNTgxMmZkZWM5NzE1M2ExNGUwYjA0NzU0NmFhIn0.ub6h1zzGL3QanxyyAlnN04r4LM2g7tHcMkLNk8VaJB0" "https://api-dsp.newweb.vn/v1/client/get-cart"''';
+    log('red ${str.bold.red.toString()}');
+    print('green ${str.bold.green.toString()}');
   });
 }
